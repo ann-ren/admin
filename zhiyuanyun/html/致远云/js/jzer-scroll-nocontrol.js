04341d01e0bd2ebf4b23a2926ea0dbf3c111db38
ren <2276744019@qq.com>
2017-05-01 17:09:17 +0800

    sdf

---
+++ zhiyuanyun/html/致远云/js/jzer-scroll-nocontrol.js
@@ -0,0 +1,59 @@
+﻿/*
+ * function：图片无间隙滚动（利用 scrollLeft 实现）
+ *
+ * author：j+2
+ *
+ * date：2013-05-02
+ */
+
+var LayerScroll = function (option) {
+    // ------------------------------ default option
+    var opt = {
+        hide: "hideLayer",
+        list: "listLayer",
+        speed: 1,
+        time: 20
+    }
+    // ------------------------------ set option
+    for (var i in option) opt[i] = option[i];
+
+    // ------------------------------ id
+    var $hide = document.getElementById(opt.hide),
+        $list = document.getElementById(opt.list);
+
+    // ------------------------------ judge
+    LayerScroll.addFloat($list, "left");
+    $list.parentNode.className += " clearfix";
+
+    var listw = $list.clientWidth;
+
+    if (listw < $hide.clientWidth) return false;
+
+    // ------------------------------ copy html
+    var $o = document.createElement($list.tagName)
+    $list.parentNode.appendChild($o).innerHTML = $list.innerHTML;
+
+    $hide.scrollLeft = 0;
+    // ------------------------------ add style
+    LayerScroll.addFloat($o, "left");
+
+    var clearTime;
+    // ------------------------------
+    var scroll = function () {
+        clearTime = setInterval(function () {
+            $hide.scrollLeft += opt.speed;
+            var nleft = $hide.scrollLeft;
+            if (listw - nleft <= 0) $hide.scrollLeft = 0;
+        }, opt.time)
+    }
+    scroll();
+    $hide.onmouseover = function () { clearInterval(clearTime) }
+    $hide.onmouseout = function () { scroll() }
+
+}
+
+LayerScroll.addFloat = function(o,value){
+    document.all ? o.style.styleFloat = value : o.style.cssFloat = value;
+}
+
+LayerScroll();
